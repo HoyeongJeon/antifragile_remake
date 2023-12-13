db2aa3813ef2f97ca9fb0a0b3ba869cefefe32f9
HoyeongJeon <hoyeong0302@gmail.com>
2023-12-13 10:03:50 +0900

    feat: 펫시터 상세조회, 펫시터 검색, public only 미들웨어 구현

--- src/controllers/petsitters.controller.js
+++ src/controllers/petsitters.controller.js
@@ -1,3 +1,5 @@
+import response from "../lib/response";
+
 export class PetsittersController {
   constructor(PetssitersService) {
     this.petsittersService = PetssitersService;
@@ -6,6 +8,37 @@ export class PetsittersController {
   getPetsitters = async (req, res, next) => {
     try {
       const responseFromService = await this.petsittersService.getPetsitters();
+      return res.status(responseFromService.status).json(responseFromService);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getPetsitterById = async (req, res, next) => {
+    try {
+      const responseFromService = await this.petsittersService.getPetsitterById(
+        req.params.petsitterId
+      );
+      return res.status(responseFromService.status).json(responseFromService);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  searchPetsitters = async (req, res, next) => {
+    try {
+      if (!req.query) {
+        return res.status(400).json(
+          response({
+            status: 400,
+            message: "검색어를 입력해주세요.",
+            data: null
+          })
+        );
+      }
+      const { keyword } = req.query;
+      const responseFromService =
+        await this.petsittersService.searchPetsitters(keyword);
       return res.status(responseFromService.status).json(responseFromService);
     } catch (error) {
       next(error);
--- src/services/petsitters.service.js
+++ src/services/petsitters.service.js
@@ -14,4 +14,42 @@ export class PetssitersService {
       data: petsitters
     });
   };
+
+  getPetsitterById = async (petsitterId) => {
+    const petsitter =
+      await this.petsittersRepository.getPetsitterById(petsitterId);
+
+    if (petsitter === null) {
+      return response({
+        status: 404,
+        message: "펫시터를 찾을 수 없습니다.",
+        data: null
+      });
+    }
+
+    return response({
+      status: 200,
+      message: "펫시터 조회에 성공했습니다.",
+      data: petsitter
+    });
+  };
+
+  searchPetsitters = async (keyword) => {
+    const petsitters =
+      await this.petsittersRepository.searchPetsitters(keyword);
+
+    if (petsitters.length === 0) {
+      return response({
+        status: 404,
+        message: "검색 결과가 없습니다.",
+        data: null
+      });
+    }
+
+    return response({
+      status: 200,
+      message: "펫시터 검색에 성공했습니다.",
+      data: petsitters
+    });
+  };
 }
